d2ea10c8d67e9a3b27632db479ff07098cedbda3
ZZZping <475377765@qq.com>
2020-03-14 22:53:30 +0800

    THE DEMO IN FIRST CHAPTER

---
+++ TigerCompilerInJava/src/IdExp.java
@@ -0,0 +1,6 @@
+public class IdExp extends Exp {
+    public String id;
+    public IdExp(String i) {
+        id = i;
+    }
+}
---
+++ TigerCompilerInJava/src/NumExp.java
@@ -0,0 +1,6 @@
+public class NumExp extends Exp {
+    public int num;
+    public NumExp(int n) {
+        num = n;
+    }
+}
